a1c8f94fc16cafe439a5412399f7ba28e8a1928f
NanreF <tyhrs887@gmail.com>
2026-04-13 00:09:16 -0300

    tudo funcionando

---
+++ meu_projeto/loja/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Fabricante)
+admin.site.register(Categoria)
+admin.site.register(Produto)
